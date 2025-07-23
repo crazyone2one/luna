85b8ca8016cac59111e9b931a07c5d4eb9aec9d3
lijinglin <the2ndindec@gmail.com>
2025-07-23 14:34:29 +0800

    style(front): 优化菜单

--- frontend/src/layout/components/BaseSidebar.vue
+++ frontend/src/layout/components/BaseSidebar.vue
@@ -7,7 +7,6 @@ import BaseIcon from '/@/components/BaseIcon.vue'
 const route = useRoute()
 const collapsed = ref(false)
 const activeMenu = ref<string>('')
-const expandedKeys = ref<string[]>([])
 const menuOptions: MenuOption[] = [
   {
     label: () => h(RouterLink, {to: {name: 'Home'}}, {default: () => 'Home'}),
@@ -71,7 +70,7 @@ const matchExpanded = (menus: MenuOption[]): boolean => {
   let matched = false
   menus.forEach(menu => {
     if (menu.children != null) {
-      matchExpanded(menu.children) && expandedKeys.value.push(menu.key as string)
+      matchExpanded(menu.children)
     }
     if (routeMatched(menu)) {
       activeMenu.value = menu.key as string
@@ -94,46 +93,43 @@ watchEffect(() => matchExpanded(menuOptions))
     <router-link to="/" #="{ navigate, href }" custom>
       <!-- prettier-ignore -->
       <n-a class="logo" :href="href" @click="navigate">
-        <svg v-if="collapsed" class="small" width="100" height="150" viewBox="0 0 100 150" xmlns="http://www.w3.org/2000/svg">
+        <svg v-if="collapsed" class="small" width="100" height="150" viewBox="0 0 100 150"
+             xmlns="http://www.w3.org/2000/svg">
           <!-- 垂直部分 -->
-          <rect x="10" y="10" width="30" height="120" fill="#9370DB" />
+          <rect x="10" y="10" width="30" height="120" fill="#9370DB"/>
           <!-- 水平部分 -->
-          <rect x="10" y="100" width="80" height="30" fill="#9370DB" />
+          <rect x="10" y="100" width="80" height="30" fill="#9370DB"/>
         </svg>
         <svg v-else width="300" height="120" viewBox="0 0 300 120" xmlns="http://www.w3.org/2000/svg">
           <!-- 字母L - 蓝色 -->
-          <rect x="10" y="20" width="25" height="80" fill="#1E90FF" />
-          <rect x="10" y="80" width="60" height="20" fill="#1E90FF" />
+          <rect x="10" y="20" width="25" height="80" fill="#1E90FF"/>
+          <rect x="10" y="80" width="60" height="20" fill="#1E90FF"/>
 
           <!-- 字母U - 绿色 -->
-          <rect x="80" y="20" width="25" height="70" fill="#32CD32" />
-          <rect x="130" y="20" width="25" height="70" fill="#32CD32" />
-          <rect x="80" y="90" width="75" height="20" fill="#32CD32" />
+          <rect x="80" y="20" width="25" height="70" fill="#32CD32"/>
+          <rect x="130" y="20" width="25" height="70" fill="#32CD32"/>
+          <rect x="80" y="90" width="75" height="20" fill="#32CD32"/>
 
           <!-- 字母N - 红色 -->
-          <rect x="170" y="20" width="25" height="100" fill="#FF4500" />
-          <rect x="210" y="20" width="25" height="100" fill="#FF4500" />
-          <polygon points="195,20 210,20 195,120 170,120" fill="#FF4500" />
+          <rect x="170" y="20" width="25" height="100" fill="#FF4500"/>
+          <rect x="210" y="20" width="25" height="100" fill="#FF4500"/>
+          <polygon points="195,20 210,20 195,120 170,120" fill="#FF4500"/>
 
           <!-- 字母A - 紫色 -->
-          <polygon points="255,20 290,70 275,70 270,50 260,50 255,70 240,70" fill="#9370DB" />
-          <rect x="240" y="70" width="50" height="20" fill="#9370DB" />
-          <rect x="260" y="90" width="10" height="30" fill="#9370DB" />
+          <polygon points="255,20 290,70 275,70 270,50 260,50 255,70 240,70" fill="#9370DB"/>
+          <rect x="240" y="70" width="50" height="20" fill="#9370DB"/>
+          <rect x="260" y="90" width="10" height="30" fill="#9370DB"/>
         </svg>
       </n-a>
     </router-link>
     <n-menu
         :value="activeMenu"
-        :default-expanded-keys="expandedKeys"
         :collapsed="collapsed"
         :collapsed-width="64"
         :collapsed-icon-size="22"
         :options="menuOptions"
-        @update:value="
-        (k: string) => {
-          activeMenu = k
-        }
-      "
+        accordion
+        @update:value="(k: string) => activeMenu = k"
     />
   </n-layout-sider>
 </template>

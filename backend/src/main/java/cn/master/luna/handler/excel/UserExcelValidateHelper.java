adc2e3617fb78c1fd68aa3c8d252ec1dca5aea56
lijinglin <the2ndindec@gmail.com>
2025-08-04 16:12:06 +0800

    refactor(backend): 用户导入功能

--- backend/src/main/java/cn/master/luna/handler/excel/UserExcelValidateHelper.java
+++ backend/src/main/java/cn/master/luna/handler/excel/UserExcelValidateHelper.java
@@ -1,6 +1,5 @@
 package cn.master.luna.handler.excel;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
@@ -15,26 +14,28 @@ import java.util.TreeSet;
  */
 @Component
 public class UserExcelValidateHelper {
-    private static UserExcelValidateHelper excelValidateHelper;
+    private static Validator validator;
 
     @Resource
-    Validator validator;
-    public static <T> String validateEntity(T obj) throws NoSuchFieldException {
+    public void setValidator(Validator validator) {
+        UserExcelValidateHelper.validator = validator;
+    }
+
+    public static <T> String validateEntity(T obj) {
+        if (validator == null) {
+            throw new IllegalStateException("Validator has not been initialized. Ensure the bean is properly created by Spring.");
+        }
         StringBuilder result = new StringBuilder();
-        Set<ConstraintViolation<T>> set = excelValidateHelper.validator.validate(obj, Default.class);
-        if (set != null && !set.isEmpty()) {
-            //报错信息进行有序、去重处理
+        Set<ConstraintViolation<T>> violations = validator.validate(obj, Default.class);
+
+        if (violations != null && !violations.isEmpty()) {
+            // 使用TreeSet进行有序和去重处理
             Set<String> errorMsgSet = new TreeSet<>();
-            for (ConstraintViolation<T> cv : set) {
-                errorMsgSet.add(cv.getMessage());
+            for (ConstraintViolation<T> violation : violations) {
+                errorMsgSet.add(violation.getMessage());
             }
             errorMsgSet.forEach(item -> result.append(item).append("; "));
         }
         return result.toString();
-    }
-    @PostConstruct
-    public void initialize() {
-        excelValidateHelper = this;
-        excelValidateHelper.validator = this.validator;
     }
 }
